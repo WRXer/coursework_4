a6a50a2553408e8bbf7a3eeac5cbf87fe8adf958
WRXer <king_311@mail.ru>
2023-03-30 12:50:14 +0400

    Написал функцию для получения данных по вакансии с хх

---
+++ main.py
@@ -0,0 +1,6 @@
+from src.engine_classes import HH, SuperJob
+
+
+if __name__ == '__main__':
+    hh = HH()
+    sj = SuperJob()
--- src/engine_classes.py
+++ src/engine_classes.py
@@ -1,6 +1,12 @@
 from abc import ABC, abstractmethod
+import requests
+
 
 class Engine(ABC):
+    def __init__(self, search_text="python", per_page=10):
+        self.search_text = search_text
+        self.per_page = per_page
+
     @abstractmethod
     def get_request(self):
         pass
@@ -13,8 +19,21 @@ class Engine(ABC):
 
 class HH(Engine):
     def get_request(self):
-        pass
+        url = "https://api.hh.ru/vacancies"
+        params = {"text": self.search_text,"per_page": self.per_page}
+
+        response = requests.get(url, params=params)
+
+        if response.status_code == 200:
+            vacancies = response.json()["items"]
+            for vacancy in vacancies:
+                print(vacancy["name"], vacancy["url"])
+        else:
+            print("Error:", response.status_code)
 
 class SuperJob(Engine):
     def get_request(self):
         pass
+
+hh = HH()
+hh.get_request()
